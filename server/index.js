017f1c793bc21debd5011f76a17e7669f183bd49
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-05-22 03:54:57 +0100

    Create React front-end

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
