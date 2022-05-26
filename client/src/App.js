e7bcc722854a13514c4a1afe17065d07c1323212
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-05-26 20:37:59 +0100

    Add GitHub Workflow to auto-format code with Prettier (#1)
    * Add GitHub Workflow to format with Prettier
    
    * Format code with Prettier
    
    Co-authored-by: IsaacCheng9 <IsaacCheng9@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
-  
+
   React.useEffect(() => {
     fetch("/api")
-    .then((res) => res.json())
-    .then((data) => setData(data.message));
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
   }, []);
 
   return (
