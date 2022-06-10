e33cc7130a07c58391f5028747567ea7e9c2eae0
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-10 03:33:57 +0100

    Update file structure to group by file type (#10)
    * Update background to match design
    
    * Update file structure to match Airbnb style guide
    
    Co-authored-by: gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>

---
+++ client/src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="bg-container">
+        <div className="bg-element bg-top-left"></div>
+        <div className="bg-element bg-top-center"></div>
+        <div className="bg-element bg-top-right"></div>
+        <div className="bg-element bg-left"></div>
+        <div className="bg-element bg-center"></div>
+        <div className="bg-element bg-right"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
