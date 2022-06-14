0e81275309cf6bc0b481c34ccfe96eb3db8cd0c4
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-14 03:46:36 +0100

    Add comment for addUserToGroup function

--- client/src/api/GroupAPI.js
+++ client/src/api/GroupAPI.js
@@ -1,3 +1,10 @@
+/** 
+ * Adds a user to a group so that expenses can be shared.
+ * @param {String} userId The unique identifier of the user to add to the group.
+ * @param {String} firstName The first name of the user.
+ * @param {String} lastName The last name of the user.
+ * @param {String} groupId The group to add the user to.
+ */
 function addUserToGroup(userId, firstName, lastName, groupId) {
   return new Promise((resolve, reject) => {
     const user = {
