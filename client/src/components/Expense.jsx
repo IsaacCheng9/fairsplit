8b20c5b791ea08117577c249f4707c643d004d19
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-06-14 21:43:51 +0100

    Create and integrate Expense component

---
+++ client/src/components/Expense.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/Expense.css";
+import smallArrow from "../assets/smallArrow.svg";
+
+function Expense(props) {
+  return (
+    <div className="expense">
+      <div className="e-date">{props.value.date}</div>
+      <div className="e-pic"></div>
+      <div className="e-title">{props.value.title}</div>
+      <div className="e-member-price">
+        <div className="e-price">£{props.value.price}</div>
+        <div className="e-members">
+          {props.value.payer}
+          <img className="e-arrow" src={smallArrow}></img>
+          {props.value.payee}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Expense;
--- client/src/components/GroupExpenses.jsx
+++ client/src/components/GroupExpenses.jsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/GroupExpenses.css";
+import Expense from "../components/Expense";
 
 function GroupExpenses(props) {
+  // Reactive array of expenses
+  let [expenses, setExpenses] = useState([]);
+
+  // Add temporary expense data to array of expenses
+  function addExpense() {
+    const updatedExpenses = [
+      ...expenses,
+      {
+        id: expenses.length,
+        date: "25th May",
+        title: "Peanut Butter",
+        payer: "Isaac",
+        payee: "George",
+        price: 3,
+      },
+    ];
+    setExpenses(updatedExpenses);
+  }
+
   return (
     <div className="group-expenses-container">
-      <div className="group-image"></div>
+      <div className="group-image">{props.value.image}</div>
       <h1 className="group-name">{props.value.name}</h1>
       <h2 className="balance group-name">
         Outstanding balance:&nbsp;
-        <span class="balance-value">
+        <span className="balance-value">
           {props.value.currency}&nbsp;
           {props.value.balance}
         </span>
       </h2>
+      <div className="expense-container">
+        {expenses.map((expense) => (
+          <Expense value={expense} key={expense.id.toString()}></Expense>
+        ))}
+      </div>
+      <div className="button-container">
+        <button className="ge-button">Settle Up</button>
+        <button className="ge-button" onClick={addExpense}>
+          Add Expense
+        </button>
+      </div>
     </div>
   );
 }
