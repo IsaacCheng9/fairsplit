1abed824df8b960c911069dc59ca0ed4da8c1498
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-14 03:33:50 +0100

    Add comments for parameters of addGroupExpense

--- client/src/api/GroupAPI.js
+++ client/src/api/GroupAPI.js
@@ -1,11 +1,21 @@
 /**
  * Creates an expense that is associated to borrowers who owe money to the
  * lender.
+ *
+ * @param {Date} creationDatetime The date and time when the expense was
+ *     created.
+ * @param {String} author The user who created the expense.
+ * @param {String} title The title stating what the expense was.
+ * @param {String} description The description of the expense.
+ * @param {Number} amount The amount of the expense in GBP.
+ * @param {String} lender The user who lent money for the expense.
+ * @param {String[]} borrowers The users who owe money for the expense.
  */
 function addGroupExpense(
   creationDatetime,
   author,
   title,
+  description,
   amount,
   lender,
   borrowers
@@ -15,6 +25,7 @@ function addGroupExpense(
       creationDatetime,
       author,
       title,
+      description,
       amount,
       lender,
       borrowers,
