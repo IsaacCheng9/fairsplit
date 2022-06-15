fe649239630eb23f1b8b26faba121bdde31296db
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-15 03:15:23 +0100

    Add model for users

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
