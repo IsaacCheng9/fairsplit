137b5d6edf73fc0a75931b08cdcff7109694a0ea
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-15 02:11:33 +0100

    Add code to connect to database

---
+++ server/db/conn.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let _db;
+
+module.expots = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verifies that we got a good database object.
+      if (db) {
+        _db = db.db("employees");
+        console.log("Successfully connected to MongoDB.");
+      }
+      return callback(err);
+    });
+  },
+
+  getDb: function () {
+    return _db;
+  },
+};
