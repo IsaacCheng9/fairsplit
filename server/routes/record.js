68c2a38cadcb1f6a3c4b9ac0132eea6d9f99d34a
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-15 02:39:51 +0100

    Add endpoints to get and create records

---
+++ server/routes/record.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+// Defines our routes and takes control of requests.
+const recordRoutes = express.Router();
+const dbo = require("../db/conn");
+// Converts the ID from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+// Gets a list of all the records.
+recordRoutes.route("/record").get(function (_, response) {
+  let dbConnect = dbo.getDb("users");
+  dbConnect
+    .collection("records")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      response.json(result);
+    });
+});
+
+// Gets a single record by ID.
+recordRoutes.route("/record/:id").get(function (request, response) {
+  let dbConnect = dbo.getDb();
+  let myQuery = { _id: ObjectId(request.params.id) };
+  dbConnect.collection("records").findOne(myQuery, function (err, result) {
+    if (err) throw err;
+    response.json(result);
+  });
+});
+
+// Creates a new record.
+recordRoutes.route("/record/add").post(function (request, response) {
+  let dbConnect = dbo.getDb();
+  let myObj = {
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+  };
+  dbConnect.collection("records").insertOne(myObj, function (err, result) {
+    if (err) throw err;
+    response.json(result);
+  });
+});
+
