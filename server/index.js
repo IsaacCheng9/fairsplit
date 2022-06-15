0a0262943eb5ac49278d9c6e4c89d3ec6670310c
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-15 03:22:43 +0100

    Change port from 5000 to 3001 for compatibility

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
