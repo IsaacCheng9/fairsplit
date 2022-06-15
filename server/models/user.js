c66f3d1afc3928eef0d5a0ea4802fe87080d568f
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-15 03:49:14 +0100

    Add max char length for first and last name

--- server/models/user.js
+++ server/models/user.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    max: [30, "First name must have fewer than 30 characters."],
     required: true,
   },
   lastName: {
     type: String,
+    max: [30, "Last name must have fewer than 30 characters."],
     required: true,
   },
 });
