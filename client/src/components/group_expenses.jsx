ec2edea5a61cda35a5ff1472b7affb3ac160af9c
George Kombostiotis <71034989+gkSideProjects@users.noreply.github.com>
2022-06-17 00:21:32 +0100

    Add components for group users view (#16)
    * Update background to match design
    
    * Update file structure to match Airbnb style guide
    
    * Add title and sign in button
    
    * Add group expenses component
    
    * Create and integrate Expense component
    
    * Add group users view
    
    * Change file names to match Google's style guide
    
    * Fix file dependencies and replace group name with 'Group Members'
    
    * Remove image from group_expenses component
    
    * Change naming convention for user types
    
    * Change centering of expense components
    
    * Update file names to match Google's style guide

---
+++ client/src/components/group_expenses.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "../styles/group_expenses.css";
+import Expense from "./expense";
+
+function GroupExpenses(props) {
+  // Reactive array of expenses
+  let [expenses, setExpenses] = useState([]);
+
+  // Add temporary expense data to array of expenses
+  function addExpense() {
+    const updatedExpenses = [
+      ...expenses,
+      {
+        id: expenses.length,
+        date: "25th May",
+        title: "Peanut Butter",
+        borrower: "Isaac",
+        lender: "George",
+        price: 3,
+      },
+    ];
+    setExpenses(updatedExpenses);
+  }
+
+  return (
+    <div className="group-expenses-container">
+      <h1 className="group-name">{props.value.name}</h1>
+      <h2 className="balance">
+        Outstanding balance:&nbsp;
+        <span className="balance-value">
+          {props.value.currency}&nbsp;
+          {props.value.balance}
+        </span>
+      </h2>
+      <div className="expense-container">
+        {expenses.map((expense) => (
+          <Expense value={expense} key={expense.id.toString()}></Expense>
+        ))}
+      </div>
+      <div className="button-container">
+        <button className="ge-button">Settle Up</button>
+        <button className="ge-button" onClick={addExpense}>
+          Add Expense
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default GroupExpenses;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import App from "./components/App";
+import App from "./components/app";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
