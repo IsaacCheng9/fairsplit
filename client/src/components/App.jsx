09b28d00773bd12e84815c9e618cf1c0fbc202b1
George Kombostiotis <71034989+gkSideProjects@users.noreply.github.com>
2022-06-17 00:04:29 +0100

    Add components for group expenses view (#15)
    * Update background to match design
    
    * Update file structure to match Airbnb style guide
    
    * Add title and sign in button
    
    * Add group expenses component
    
    * Create and integrate Expense component
    
    Co-authored-by: Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import arrow from "../assets/arrow.svg";
 import "../styles/App.css";
+import GroupExpenses from "../components/GroupExpenses";
+
+// Temporary group to display component data
+let group = {
+  name: "4 Portal Road",
+  balance: 24,
+  currency: "£",
+};
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -12,6 +21,14 @@ function App() {
 
   return (
     <div className="App">
+      <div className="header-container">
+        <h1 className="title">SplitWise</h1>
+        <a href={"#"} className="sign-in">
+          Sign in
+          <img className="arrow" src={arrow} />
+        </a>
+      </div>
+
       <div className="bg-container">
         <div className="bg-element bg-top-left"></div>
         <div className="bg-element bg-top-center"></div>
@@ -20,6 +37,8 @@ function App() {
         <div className="bg-element bg-center"></div>
         <div className="bg-element bg-right"></div>
       </div>
+
+      <GroupExpenses value={group}></GroupExpenses>
     </div>
   );
 }
