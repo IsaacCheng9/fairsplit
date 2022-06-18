389ff5902dd61457c9720a3df7ee93aedf7ca34f
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-18 03:43:31 +0100

    Create expenseSchema

--- server/models/expense.js
+++ server/models/expense.js
@@ -1,3 +1,27 @@
 const mongoose = require("mongoose");
 
-const ExpenseSchema = new mongoose.Schema({});
+const expenseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    max: [50, "Title must have fewer than 50 characters."],
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  creationDatetime: {
+    type: Date,
+    default: Date.now,
+  },
+  lender: {
+    type: String,
+    required: true,
+  },
+  borrowers: {
+    type: [String],
+    required: true,
+  },
+});
+
+module.exports = User = mongoose.model("expense", expenseSchema);
