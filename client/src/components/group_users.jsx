a782c07edf71c2f0e8012d4c3a9282249211ea7c
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-06-19 10:34:51 +0100

    Call 'users' route from 'group_users' component

--- client/src/components/group_users.jsx
+++ client/src/components/group_users.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import "../styles/group_users.css";
 
 function GroupUsers(props) {
+  // API URL
+  let apiUrl = "http://localhost:3001";
+
+  async function getUsers() {
+    let response = await fetch(apiUrl + "/users");
+    console.log(response);
+  }
+
+  // Use hook to test getUsers()
+  useEffect(() => {
+    getUsers();
+  })
+
   return (
     <div className="users-container">
       <h1 className="users-title">Group Members</h1>
