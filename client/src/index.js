d7f3696dfe284fed760080fb8db6ac10e965e3ed
gkSideProjects <gkSideProjects@users.noreply.github.com>
2022-06-19 10:00:11 +0000

    Format code with Prettier

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,7 @@ import App from "./components/app";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <App />
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
