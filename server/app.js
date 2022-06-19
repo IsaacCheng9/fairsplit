a3a2999664a97fd14b0f3d9e780944c1c5bbc9af
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-19 03:30:31 +0100

    Add connection to MongoDB using Mongoose

---
+++ server/app.js
@@ -0,0 +1,14 @@
+// Sets up the Mongoose connection.
+let mongoose = require("mongoose");
+// Remember to replace <password> with the actual credential locally.
+let devUrl =
+  "mongodb+srv://admin:<password>@fairsplit.fjvgxmg.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(devUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+let db = mongoose.connection;
+
+// Binds the connection to an error event to get notification of connection
+// errors.
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
