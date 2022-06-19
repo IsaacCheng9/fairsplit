8c4a88474f96a117dccd2bcfb86abd965f9bd9c1
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-19 03:50:32 +0100

    Add message for successful MongoDB connection

--- server/app.js
+++ server/app.js
@@ -12,3 +12,7 @@ let db = mongoose.connection;
 // Binds the connection to an error event to get notification of connection
 // errors.
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// Displays a success message when the connection is successfully made.
+db.once("open", function () {
+  console.log("Connected successfully.");
+});
