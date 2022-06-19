e81cc0ef6ae0d18f042601762fc8dfc2d04f367d
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-19 15:45:22 +0100

    Remove router until it's later implemented

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const router = require("./routes");
+// const router = require("./routes");
 const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
-app.use(router);
+// app.use(router);
 
 // IMPORTANT: Replace this value with the actual password locally.
 const password = "<password>";
