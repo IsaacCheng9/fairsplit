e457696548bd63b81798515929b7813028b513a7
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-06-19 09:58:21 +0100

    Add CORS to allow frontend to talk to backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config({ path: "./config.env" });
 const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
 
