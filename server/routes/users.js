d4d5efa1ac6f05852b6e80a4738bc9c4e6d5f07c
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-06-21 20:22:13 +0100

    Remove redundant module

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const userModel = require("../models/user");
 const app = express();
-const mongoose = require("mongoose");
 
 // Gets a list of all the users.
 app.get("/users", async (_, response) => {
