4f952e81d5cc3d8f1e593813f46793174dceed5f
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-21 20:27:21 +0100

    Implement routes to add a user, view a user, and view all users (#12)
    * Create function to add a user to the group
    
    * Remake function to add a user to a group
    
    * Add comment for addUserToGroup function
    
    * Format code with Prettier
    
    * Remove old files
    
    * Add mongodb, express, cors, dotenv
    
    * Add setup for MongoDB connection
    
    * Ignore MongoDB connection config file
    
    * Add code to connect to database
    
    * Change DB objects from 'employees' to 'users'
    
    * Add endpoints to get and create records
    
    * Format code with Prettier
    
    * Add route to update record by ID
    
    * Add route to delete user
    
    * Refactor code to be specific to users
    
    * Rename file to users.js
    
    * Refactor variable names
    
    * Change port to 3001
    
    * Add body-parser, mongoose, validation dependencies
    
    * Add model for users
    
    * Format code with Prettier
    
    * Delete old GroupAPI file
    
    * Add max char length for first and last name
    
    * Add contributors
    
    * Fix variable name for consistency
    
    * Fix typo
    
    * Use users route
    
    * Format code with Prettier
    
    * Add CORS to allow frontend to talk to backend
    
    * Call 'users' route from 'group_users' component
    
    * Format code with Prettier
    
    * Remove StrictMode so hooks aren't run more than once
    
    * Add CORS to package.json
    
    * Call 'user by id' route from group_user component - use hook to test
    
    * Add CORS dependency
    
    * Remove fetch to non-existent api
    
    * Refactor 'get users' route to use mongoose
    
    * Format code with Prettier
    
    * Remove unused variable
    
    * Remove incorrect old code
    
    * Remove duplicate route for getting all users
    
    * Add creationDatetime for tracking
    
    * Add route to create a new user
    
    * Update non-functional routes
    
    * Format code with Prettier
    
    * Remove unused variable declaration
    
    * Add user JSON response if successfully added
    
    * Use route for users
    
    * Fix route from singular to plural 'users'
    
    * Format code with Prettier
    
    * Add get user by ID route
    
    * Fetch get user by id route using Get
    
    * Format code with Prettier
    
    * Fix route to get users (singular -> plural)
    
    * Change responses to provide JSON
    
    * Fix route to use singular '/users/id' route
    
    * Pass in valid argument to /users/id route
    
    * Refactor code, remove unused variable
    
    * Remove redundant module
    
    * Fix title from 'SplitWise' to 'FairSplit'
    
    * Remove comment
    
    Co-authored-by: IsaacCheng9 <IsaacCheng9@users.noreply.github.com>
    Co-authored-by: gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
    Co-authored-by: gkSideProjects <gkSideProjects@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-// const router = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const usersRouter = require("./routes/users.js");
 
 const app = express();
 app.use(express.json());
-// app.use(router);
+app.use(cors());
+app.use(usersRouter);
 
 // IMPORTANT: Replace this value with the actual password locally.
 const password = "<password>";
