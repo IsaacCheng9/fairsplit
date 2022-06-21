78cf88f65141088046c68ab271ce88ae1199f2cf
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-06-21 04:05:17 +0100

    Remove incorrect old code

--- server/routes/users.js
+++ server/routes/users.js
@@ -17,66 +17,22 @@ app.get("/users", async (request, response) => {
 /*
 // Gets a list of all the users.
 userRoutes.route("/users").get(function (_, response) {
-  let dbConnect = dbo.getDb("users");
-  dbConnect
-    .collection("records")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      response.json(result);
-    });
 });
 
 // Gets a user by ID.
 userRoutes.route("/user/:id").get(function (request, response) {
-  let dbConnect = dbo.getDb();
-  let myQuery = { _id: objectId(request.params.id) };
-  dbConnect.collection("records").findOne(myQuery, function (err, result) {
-    if (err) throw err;
-    response.json(result);
-  });
 });
 
 // Creates a new user.
 userRoutes.route("/user/add").post(function (request, response) {
-  let dbConnect = dbo.getDb();
-  let myObj = {
-    firstName: request.body.firstName,
-    lastName: request.body.lastName,
-  };
-  dbConnect.collection("records").insertOne(myObj, function (err, result) {
-    if (err) throw err;
-    response.json(result);
-  });
 });
 
 // Updates a user by ID.
 userRoutes.route("/user/update/:id").post(function (request, response) {
-  let dbConnect = dbo.getDb();
-  let myQuery = { _id: objectId(request.params.id) };
-  let newValues = {
-    $set: {
-      firstName: request.body.firstName,
-      lastName: request.body.lastName,
-    },
-  };
-  dbConnect
-    .collection("records")
-    .updateOne(myQuery, newValues, function (err, result) {
-      if (err) throw err;
-      response.json(result);
-    });
 });
 
 // Deletes a user by ID.
 userRoutes.route("/user/delete/:id").delete(function (request, response) {
-  let dbConnect = dbo.getDb();
-  let myQuery = { _id: objectId(request.params.id) };
-  dbConnect.collection("records").deleteOne(myQuery, function (err, result) {
-    if (err) throw err;
-    console.log("One user deleted.");
-    response.json(result);
-  });
 });
 */
 
