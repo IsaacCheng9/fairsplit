4864eaeeeb4d26b0d4784dd0d12bc1bead738cc9
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-06-26 16:29:52 +0100

    Implement add_user component into group_users component

--- client/src/components/group_users.jsx
+++ client/src/components/group_users.jsx
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import "../styles/group_users.css";
 import User from "./user";
+import Add_User from "./add_user";
 
 function GroupUsers(props) {
   // API URL
   let apiUrl = "http://localhost:3001";
-
   let users = props.value.users;
+  let [addedUsers, setAddedUsers] = useState(users);
+
+  function addUser(user) {
+    const newUser = { username: user, indebted: false, balance: 0 };
+    setAddedUsers([...addedUsers, newUser]);
+  }
 
   async function getUsers() {
     let response = await fetch(apiUrl + "/users");
@@ -23,13 +29,20 @@ function GroupUsers(props) {
   useEffect(() => {
     getUsers();
     getUserById("62b1edee24f619f5f5066f37");
-  }, [])
+  }, []);
 
   return (
     <div className="group-members-container">
       <h1 className="group-members-title">Group Members</h1>
       <div className="users-container">
-        {users.map((user) => (<User value={user} key={user.username}></User>))}
+        {addedUsers.map((user) => (
+          <User value={user} key={user.username}></User>
+        ))}
+        <Add_User
+          onClick={(user) => {
+            addUser(user);
+          }}
+        ></Add_User>
       </div>
     </div>
   );
