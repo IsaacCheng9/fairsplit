0227b0436c435ff32e27dea4d30065a7a90fcabe
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-06-29 21:25:29 +0100

    Create div with no overflow to fit expense form and add reactive variables for styling

--- client/src/components/add_expense.jsx
+++ client/src/components/add_expense.jsx
@@ -3,48 +3,70 @@ import "../styles/add_expense.css";
 
 function AddExpense(props) {
   // Reactive variables for dynamic styling
-  let [containerClass, setContainerClass] = useState("add-expense-container add-expense-container-shrink");
+  let [containerClass, setContainerClass] = useState("add-expense-container");
   let [plusClass, setPlusClass] = useState("add-expense-plus");
+  let [fristClass, setFirstClass] = useState("add-expense-title");
+  let [secondClass, setSecondClass] = useState("title-input");
+  let [overflowClass, setOverflowClass] = useState("overflow-container");
 
   // Expands add-expense container to make room for input fields
   function expandContainer() {
-    setContainerClass("add-expense-container add-expense-container-expand")
-    setPlusClass("add-expense-plus add-expense-plus-expand")
+    if (overflowClass.includes("expand")) {
+      setOverflowClass("overflow-container");
+    } else {
+      setContainerClass("add-expense-container add-expense-container-expand");
+      setFirstClass("add-expense-title add-expense-title-expand");
+      setSecondClass("title-input title-input-expand");
+      setOverflowClass("overflow-container overflow-container-expand");
+      props.onClick();
+    }
   }
 
   return (
-    <div className={containerClass}>
-      <div className={plusClass} onClick={expandContainer}>
-        <svg
-          width="30"
-          height="30"
-          viewBox="0 0 100 100"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g id="Frame 1">
-            <g id="cross">
-              <line
-                id="y-line"
-                x1="50.5"
-                y1="15"
-                x2="50.5"
-                y2="86"
-                stroke="black"
-                strokeWidth="4"
-              />
-              <line
-                id="x-line"
-                x1="14"
-                y1="49.5"
-                x2="85"
-                y2="49.5"
-                stroke="black"
-                strokeWidth="4"
-              />
+    <div>
+      <div className={overflowClass}>
+        <heading className={fristClass}>Title</heading>
+        <input className={secondClass}></input>
+        <heading className="add-expense-author">Author</heading>
+        <input className="author-input"></input>
+        <heading className="add-expense-lender">Lender</heading>
+        <input className="lender-input"></input>
+        <heading className="add-expense-borrower">Borrower</heading>
+        <input className="borrower-input"></input>
+      </div>
+      <div className={containerClass}>
+        <div className={plusClass} onClick={expandContainer}>
+          <svg
+            width="30"
+            height="30"
+            viewBox="0 0 100 100"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="Frame 1">
+              <g id="cross">
+                <line
+                  id="y-line"
+                  x1="50.5"
+                  y1="15"
+                  x2="50.5"
+                  y2="86"
+                  stroke="black"
+                  strokeWidth="4"
+                />
+                <line
+                  id="x-line"
+                  x1="14"
+                  y1="49.5"
+                  x2="85"
+                  y2="49.5"
+                  stroke="black"
+                  strokeWidth="4"
+                />
+              </g>
             </g>
-          </g>
-        </svg>
+          </svg>
+        </div>
       </div>
     </div>
   );
