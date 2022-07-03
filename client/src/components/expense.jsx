1910762aa3a8fff85c5ce2eae20d91b2e742ecae
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-07-03 16:19:33 +0100

    Format date and borrowers in expense component

--- client/src/components/expense.jsx
+++ client/src/components/expense.jsx
@@ -5,14 +5,16 @@ import smallArrow from "../assets/small_arrow.svg";
 function Expense(props) {
   return (
     <div className="expense">
-      <div className="e-date">{props.value.date}</div>
+      <div className="e-date">
+        {props.value.creationDatetime.substring(0, 10)}
+      </div>
       <div className="e-pic"></div>
       <div className="e-title">{props.value.title}</div>
       <div className="e-member-price">
         <div className="e-price">£{props.value.price}</div>
         <div className="e-members">
-          {props.value.borrower}
-          <img className="e-arrow" src={smallArrow}></img>
+          {props.value.borrowers.join(", ")}
+          <img alt="arrow" className="e-arrow" src={smallArrow}></img>
           {props.value.lender}
         </div>
       </div>
