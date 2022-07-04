39a83d5d6fa89a9109b0db06db1403e731a6fdb1
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-04 09:48:29 +0100

    Implement routes to view and create debts between users (#28)
    * Add debt schema to track balances between users
    
    * Add route for handling debts
    
    * Change routes for increasing/decreasing debt
    
    * Fix double curly braces in catch statement
    
    * Remove unnecessary '= User ='

---
+++ server/routes/debts.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const debtModel = require("../models/debt");
+const app = express();
+
+// Gets a list of all debts.
+app.get("/debts", async (_, response) => {
+  const debt = await debtModel.find({});
+
+  try {
+    response.json(debt);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+// Gets a debt by ID.
+app.get("/debts/:id", async (request, response) => {
+  const debt = await debtModel.findById(request.params.id);
+
+  try {
+    response.json(debt);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+// Creates a new debt between two users.
+app.post("/debts/add", async (request, response) => {
+  const debt = await debtModel.create({
+    from: request.body.from,
+    to: request.body.to,
+    amount: request.body.amount,
+  });
+
+  try {
+    response.json(debt);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+// Increases the amount of debt by ID to represent the user borrowing money.
+// app.put("/debts/increase/:id", async (request, response) => {});
+
+// Decreases the amount of debt by ID to represent the user paying their debt.
+// app.put("/debts/decrease/:id", async (request, response) => {});
+
+// Deletes a debt by ID.
+// app.delete("/debts/delete/:id", async (request, response) => {});
+
+module.exports = app;
