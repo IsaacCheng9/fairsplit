0b34f8c80f546ff3138409f6aa86cc12ac946969
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-17 03:50:00 +0100

    Add debt controller file

---
+++ server/controllers/debt_controller.js
@@ -0,0 +1,40 @@
+var debtModel = require("../models/debt");
+
+// Gets a list of all debts.
+exports.getAllDebts = async (request, response) => {
+  const debt = await debtModel.find({});
+
+  try {
+    response.json(debt);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+};
+
+// Gets a debt by ID.
+exports.getDebtById = async (request, response) => {
+  const debt = await debtModel.findById(request.params.id);
+
+  try {
+    response.json(debt);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+};
+
+// Creates a new debt between two users.
+exports.createNewDebt = async (request, response) => {
+  const debt = await debtModel.create({
+    from: request.body.from,
+    to: request.body.to,
+    amount: request.body.amount,
+  });
+
+  try {
+    response.json(debt);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+};
+
+exports.addDebt = async (request, response) => {};
