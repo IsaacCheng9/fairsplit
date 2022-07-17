83013b07707060fde4e7fbc3fe1435d7d5b0d1a2
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-17 04:17:50 +0100

    Update future expense routes to use controllers

--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -3,54 +3,15 @@ const expenseController = require("../controllers/expense_controller");
 const app = express();
 
 // Gets all expenses in the group.
-// app.get("/expenses", async (_, response) => {
-//   const expenses = await expenseModel.find();
-
-//   try {
-//     response.json(expenses);
-//   } catch (error) {
-//     response.status(500).send(error);
-//   }
-// });
-
-// Gets an expense by ID.
-// app.get("/expenses/:id", async (request, response) => {
-//   const expenses = await expenseModel.findById(request.params.id);
-
-//   try {
-//     response.json(expenses);
-//   } catch (error) {
-//     response.status(500).send(error);
-//   }
-// });
+// app.get("/expenses", expenseController.getExpenses);
 
 // Adds an expense.
 app.post("/expenses/add", expenseController.addExpense);
 
 // Updates an expense by ID.
-// app.put("/expenses/update/:id", async (request, response) => {
-//   const expenses = await expenseModel.findByIdAndUpdate(request.params.id, {
-//     title: request.body.title,
-//     lender: request.body.lender,
-//     borrowers: request.body.borrowers,
-//   });
-
-//   try {
-//     response.json(expenses);
-//   } catch (error) {
-//     response.status(500).send(error);
-//   }
-// });
+// app.put("/expenses/update/:id", expenseController.updateExpenseById);
 
 // Deletes an expense by ID.
-// app.delete("/expenses/delete/:id", async (request, response) => {
-//   const expenses = await expenseModel.findByIdAndDelete(request.params.id);
-
-//   try {
-//     response.json(expenses);
-//   } catch (error) {
-//     response.status(500).send(error);
-//   }
-// });
+// app.delete("/expenses/delete/:id", expenseController.deleteExpense);
 
 module.exports = app;
