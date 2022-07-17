a0618a0f983e7a07d72f2b56950a6e192bdd9922
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-07-17 15:12:11 +0100

    Make temporary group reactive with useState hook

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,36 +1,43 @@
 import React from "react";
+import { useState } from "react";
 import arrow from "../assets/arrow.svg";
 import "../styles/app.css";
 import GroupExpenses from "./group_expenses";
 import GroupUsers from "./group_users";
 
-// Temporary group to display component data
-let group = {
-  name: "4 Portal Road",
-  balance: 0,
-  currency: "£",
-  users: [
-    { username: "Jim", indebted: true, balance: 14 },
-    { username: "Bob", indebted: false, balance: 4 },
-    { username: "Joe", indebted: false, balance: 7 },
-    { username: "Jane", indebted: false, balance: 3 },
-  ],
-};
-
-// Calculate total balance of group
-group["balance"] = calculateTotalBalance();
-
-function calculateTotalBalance() {
-  let totalBalance = 0;
-  group.users.forEach((user) => {
-    if (user.indebted) {
-      totalBalance -= user.balance;
-    }
-  });
-  return totalBalance;
-}
-
 function App() {
+  // Temporary group to display component data
+  let [group, setGroup] = useState({
+    name: "4 Portal Road",
+    balance: 0,
+    currency: "£",
+    users: [
+      { username: "Jim", indebted: true, balance: 14 },
+      { username: "Bob", indebted: false, balance: 4 },
+      { username: "Joe", indebted: false, balance: 7 },
+      { username: "Jane", indebted: false, balance: 3 },
+    ],
+  });
+
+  // Calculate total balance of group
+  group["balance"] = calculateTotalBalance();
+
+  function calculateTotalBalance() {
+    let totalBalance = 0;
+    group.users.forEach((user) => {
+      if (user.indebted) {
+        totalBalance -= user.balance;
+      }
+    });
+    return totalBalance;
+  }
+
+  function updateGroup(user) {
+    let newGroup = { ...group };
+    newGroup.users.push(user);
+    setGroup(newGroup);
+  }
+
   return (
     <div className="App">
       <div className="header-container">
@@ -52,7 +59,7 @@ function App() {
 
       <div className="main-content-container">
         <GroupExpenses value={group}></GroupExpenses>
-        <GroupUsers value={group}></GroupUsers>
+        <GroupUsers value={group} onClick={updateGroup}></GroupUsers>
       </div>
     </div>
   );
