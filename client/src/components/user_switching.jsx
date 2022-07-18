7c6baafb4c2ecdff49b3b5491f7abb1bb1039b4d
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-07-18 20:10:10 +0100

    Add styling to user switching component and render users as options in select tag

--- client/src/components/user_switching.jsx
+++ client/src/components/user_switching.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import user_switching from "../styles/user_switching.css";
 
 function UserSwitching(props) {
+  let users = props.value.users;
+
   return (
     <section className="user-switching-container">
       <select name="users">
-        {props.value.users.map((user) => {
-          <option key={user.username}>{user.username}</option>;
-        })}
+        {users.map((user) => (
+          <option key={user.username}>{user.username}</option>
+        ))}
       </select>
     </section>
   );
