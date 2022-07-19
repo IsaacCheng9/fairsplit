1663b690bea4a8a0c794404b3dc37862168ccd74
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-19 22:27:11 +0100

    Clean up code for adding an expense

--- server/controllers/expense_controller.js
+++ server/controllers/expense_controller.js
@@ -1,4 +1,5 @@
 const helpers = require("./helpers/index");
+const debtModel = require("../models/debt");
 const expenseModel = require("../models/expense");
 
 // Adds an expense.
@@ -12,39 +13,34 @@ exports.addExpense = async (request, response) => {
       borrowers: request.body.borrowers,
       amount: request.body.amount,
     });
+
+    // TODO: Loop through so that it works for all borrowers instead of just one.
+    // Check whether the debt exists between two users so that we can either
+    // create a new debt, or update an existing debt.
+    const debtExists = await debtModel.exists({
+      from: request.body.borrowers[0],
+      to: request.body.lender,
+    });
+
+    // TODO: Fix this – it currently does nothing when a debt already exists.
+    if (debtExists) {
+      // Update the debt between the lender and borrower.
+      helpers.updateDebt(
+        request.body.borrowers[0],
+        request.body.lender,
+        request.body.amount
+      );
+    } else {
+      // Create a new debt between the lender and borrower.
+      helpers.createDebt(
+        request.body.borrowers[0],
+        request.body.lender,
+        request.body.amount
+      );
+    }
+
     response.json(expense);
   } catch (error) {
     response.status(500).send(error);
-  }
-
-  // Check whether the debt exists between two users so that we can either
-  // create a new debt, or update an existing debt.
-  const debtExists = await debtModel.exists({
-    from: request.body.from,
-    to: request.body.to,
-  });
-
-  if (debtExists) {
-    // Update the debt between the lender and borrower.
-    try {
-      helpers.updateDebt(
-        request.body.lender,
-        request.body.borrowers,
-        request.body.amount
-      );
-    } catch (error) {
-      response.status(500).send(error);
-    }
-  } else {
-    // Create a new debt between the lender and borrower.
-    try {
-      helpers.createDebt(
-        request.body.lender,
-        request.body.borrowers,
-        request.body.amount
-      );
-    } catch (error) {
-      response.status(500).send(error);
-    }
   }
 };
