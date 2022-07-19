949fc2b025d73b8f7594982515d5fb1d48ba1f3a
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-19 20:56:11 +0100

    Implement route to settle a debt (#35)
    * Change route to /settle/:id
    
    * Add debt controller to settle debt
    
    * Remove ID from route for settling debt
    
    * Fix type of HTTP method from DELETE to POST
    
    * Add catch for <0 amount paid
    
    * Remove try-catch, add response 400
    
    * Simplify try-catch statements

--- server/controllers/debt_controller.js
+++ server/controllers/debt_controller.js
@@ -2,9 +2,8 @@ const debtModel = require("../models/debt");
 
 // Gets a list of all debts.
 exports.getDebts = async (_, response) => {
-  const debt = await debtModel.find({});
-
   try {
+    const debt = await debtModel.find({});
     response.json(debt);
   } catch (error) {
     response.status(500).send(error);
@@ -13,9 +12,8 @@ exports.getDebts = async (_, response) => {
 
 // Gets a debt by ID.
 exports.getDebtById = async (request, response) => {
-  const debt = await debtModel.findById(request.params.id);
-
   try {
+    const debt = await debtModel.findById(request.params.id);
     response.json(debt);
   } catch (error) {
     response.status(500).send(error);
@@ -33,33 +31,78 @@ exports.addDebt = async (request, response) => {
 
   if (debtExists) {
     // Update an existing debt.
-    await debtModel.findOneAndUpdate(
-      {
-        from: request.body.from,
-        to: request.body.to,
-      },
-      {
-        $inc: { amount: request.body.amount },
-      }
-    );
-
     try {
+      await debtModel.findOneAndUpdate(
+        {
+          from: request.body.from,
+          to: request.body.to,
+        },
+        {
+          $inc: { amount: request.body.amount },
+        }
+      );
       response.send("Debt updated successfully.");
     } catch (error) {
       response.status(500).send(error);
     }
   } else {
     // Create a new debt between the two users.
-    const debt = await debtModel.create({
-      from: request.body.from,
-      to: request.body.to,
-      amount: request.body.amount,
-    });
-
     try {
+      const debt = await debtModel.create({
+        from: request.body.from,
+        to: request.body.to,
+        amount: request.body.amount,
+      });
       response.json(debt);
     } catch (error) {
       response.status(500).send(error);
     }
+  }
+};
+
+// Settles a debt by ID.
+exports.settleDebt = async (request, response) => {
+  // Search for a debt by swapping the from and to fields, as a settlement is
+  // effectively a reverse debt.
+  const existingDebt = await debtModel.findOne({
+    from: request.body.to,
+    to: request.body.from,
+  });
+
+  if (request.body.amount <= 0) {
+    response.status(400).send("Amount must be greater than £0.");
+  } else if (existingDebt && existingDebt.amount > request.body.amount) {
+    // If the existing debt is greater than the amount to be settled, then
+    // reduce the debt.
+    try {
+      await debtModel.findOneAndUpdate(
+        {
+          from: request.body.to,
+          to: request.body.from,
+        },
+        {
+          $inc: { amount: -request.body.amount },
+        }
+      );
+      response.send("Debt partially settled and reduced successfully.");
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  } else if (existingDebt && existingDebt.amount === request.body.amount) {
+    try {
+      // If the existing debt is equal to the amount to be settled, then delete
+      // the debt.
+      await debtModel.findOneAndDelete({
+        from: request.body.to,
+        to: request.body.from,
+      });
+      response.send("Debt fully settled and deleted successfully.");
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  } else {
+    response
+      .status(400)
+      .send("You cannot settle more than the amount of the debt.");
   }
 };
