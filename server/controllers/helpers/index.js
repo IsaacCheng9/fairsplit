ca2c8ee5aa051779599a38f0e8eef946b3dca0c2
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-19 22:01:09 +0100

    Add updateDebt helper function

--- server/controllers/helpers/index.js
+++ server/controllers/helpers/index.js
@@ -1,14 +1,32 @@
 const debtModel = require("../../models/debt");
 
 // Create a new debt between the two users.
-exports.createNewDebt = function(from, to, amount) {
+exports.createNewDebt = function (from, to, amount) {
   try {
-    const debt = await debtModel.create({
+    const debt = debtModel.create({
       from: from,
       to: to,
-      amount: amount
+      amount: amount,
     });
     response.json(debt);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+};
+
+// Update an existing debt.
+exports.updateDebt = function (from, to, amount) {
+  try {
+    debtModel.findOneAndUpdate(
+      {
+        from: from,
+        to: to,
+      },
+      {
+        $inc: { amount: amount },
+      }
+    );
+    response.send("Debt updated successfully.");
   } catch (error) {
     response.status(500).send(error);
   }
