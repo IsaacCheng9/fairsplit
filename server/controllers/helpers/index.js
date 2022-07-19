f73551abef963da58661285809664985bdc76081
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-19 22:05:28 +0100

    Remove try-catch

--- server/controllers/helpers/index.js
+++ server/controllers/helpers/index.js
@@ -2,32 +2,22 @@ const debtModel = require("../../models/debt");
 
 // Create a new debt between the two users.
 exports.createNewDebt = function (from, to, amount) {
-  try {
-    const debt = debtModel.create({
-      from: from,
-      to: to,
-      amount: amount,
-    });
-    response.json(debt);
-  } catch (error) {
-    response.status(500).send(error);
-  }
+  const debt = debtModel.create({
+    from: from,
+    to: to,
+    amount: amount,
+  });
 };
 
 // Update an existing debt.
 exports.updateDebt = function (from, to, amount) {
-  try {
-    debtModel.findOneAndUpdate(
-      {
-        from: from,
-        to: to,
-      },
-      {
-        $inc: { amount: amount },
-      }
-    );
-    response.send("Debt updated successfully.");
-  } catch (error) {
-    response.status(500).send(error);
-  }
+  debtModel.findOneAndUpdate(
+    {
+      from: from,
+      to: to,
+    },
+    {
+      $inc: { amount: amount },
+    }
+  );
 };
