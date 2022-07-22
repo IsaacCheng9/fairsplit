d8d4946b0dbbb4fb56d4722fdf6f015621b9546d
George Kombostiotis <georgekombo99@hotmail.com>
2022-07-22 12:47:17 +0100

    Pass user selected back to root component to update active user

--- client/src/components/user_switching.jsx
+++ client/src/components/user_switching.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import user_switching from "../styles/user_switching.css";
+import { createRef } from "react";
 
 function UserSwitching(props) {
-  let users = props.value.users;
+  let users = props.users;
+  let selectRef = createRef();
+
+  function handleChange() {
+    let index = props.value.filter((user) => {
+      return user.username === selectRef.current.value;
+    });
+
+    props.onClick(selectRef.current.value, props.value.indexOf(index[0]));
+  }
 
   return (
     <section className="user-switching-container">
-      <select name="users">
+      <select name="users" ref={selectRef} onChange={handleChange}>
         {users.map((user) => (
           <option key={user.username}>{user.username}</option>
         ))}
