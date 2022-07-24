6f4b0c18cc83ef5507e4878e4569b5974c0bcfc3
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-24 22:28:19 +0100

    Add cancellation of reverse debt

--- server/controllers/debt_controller.js
+++ server/controllers/debt_controller.js
@@ -23,33 +23,66 @@ exports.getDebtById = async (request, response) => {
 
 // Adds a debt between two users.
 exports.addDebt = async (request, response) => {
-  // Check whether the debt exists between two users so that we can either
-  // create a new debt, or update an existing debt.
-  const debtExists = await debtModel.exists({
-    from: request.body.from,
-    to: request.body.to,
+  // Check whether the debt exists the other way around, as this new debt may
+  // cancel out the reverse debt.
+  const reverseDebt = await debtModel.findOne({
+    from: request.body.to,
+    to: request.body.from,
   });
+  // Keep track of the debt amount to add, as this may change if there is a
+  // reverse debt to be settled.
+  let debtAmount = request.body.amount;
+  // If the reverse debt is greater than the new debt, then reduce the reverse
+  // debt and avoid adding a new debt.
+  if (reverseDebt && reverseDebt.amount > request.body.amount) {
+    await debtModel.findOneAndUpdate(
+      {
+        from: request.body.to,
+        to: request.body.from,
+      },
+      {
+        $inc: { amount: -request.body.amount },
+      }
+    );
+    debtAmount = 0;
+  } else if (reverseDebt && reverseDebt.amount <= request.body.amount) {
+    // If the reverse debt is less than or equal to the new debt, then delete
+    // the reverse debt and update the new debt amount.
+    debtAmount -= reverseDebt.amount;
+    await debtModel.findOneAndDelete({
+      from: request.body.to,
+      to: request.body.from,
+    });
+  }
 
-  try {
-    if (debtExists) {
-      // Update the debt between the lender and borrower.
-      helpers.updateDebt(
-        request.body.from,
-        request.body.to,
-        request.body.amount
-      );
-      response.send("Debt updated successfully.");
-    } else {
-      // Create a new debt between the lender and borrower.
-      helpers.createDebt(
-        request.body.from,
-        request.body.to,
-        request.body.amount
-      );
-      response.send("Debt created successfully.");
+  // If the reverse debt has cancelled out the new debt, then don't add a new
+  // debt. Otherwise, proceed to add the new debt.
+  if (debtAmount === 0) {
+    response.send(
+      `The new debt was used to cancel out a reverse debt, so a new debt from
+      '${request.body.from}' to '${request.body.to}' was not added.`
+    );
+  } else {
+    // Check whether the debt exists between two users so that we can either
+    // create a new debt, or update an existing debt.
+    const debtExists = await debtModel.exists({
+      from: request.body.from,
+      to: request.body.to,
+    });
+
+    try {
+      if (debtExists) {
+        // Update the debt between the lender and borrower.
+        helpers.updateDebt(request.body.from, request.body.to, debtAmount);
+        response.send("Debt updated successfully.");
+      } else {
+        // Create a new debt between the lender and borrower.
+        helpers.createDebt(request.body.from, request.body.to, debtAmount);
+        response.send("Debt created successfully.");
+      }
+    } catch (error) {
+      response.status(500).send(error);
     }
-  } catch (error) {
-    response.status(500).send(error);
   }
 };
 
