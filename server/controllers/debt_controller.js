1ac7250e7215749ac9e54ee550cd102134d4b372
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-25 20:13:26 +0100

    Simplify debts between two users when a new debt is added (#43)
    * Add cancellation of reverse debt
    
    * Improve response messages with specific info
    
    * Fix tense in comments
    
    * Add helper function for addDebt
    
    * Fix extra space
    
    * Rename helper function to processNewDebt
    
    * Call helper processNewDebt to avoid repetition
    
    * Remove unnecessary try-catch loops
    
    * Remove unnecessary updateDebt and createDebt functions
    
    * Add note to improve client response message later
    
    * Send specific message as client response
    
    Co-authored-by: George Kombostiotis <71034989+gkSideProjects@users.noreply.github.com>
    
    * Remove old TODO comment
    
    Co-authored-by: George Kombostiotis <71034989+gkSideProjects@users.noreply.github.com>

--- server/controllers/debt_controller.js
+++ server/controllers/debt_controller.js
@@ -1,59 +1,29 @@
 const debtModel = require("../models/debt");
 const helpers = require("./helpers/index");
 
-// Gets a list of all debts.
+// Get a list of all debts.
 exports.getDebts = async (_, response) => {
-  try {
-    const debt = await debtModel.find({});
-    response.json(debt);
-  } catch (error) {
-    response.status(500).send(error);
-  }
+  const debt = await debtModel.find({});
+  response.json(debt);
 };
 
-// Gets a debt by ID.
+// Get a debt by ID.
 exports.getDebtById = async (request, response) => {
-  try {
-    const debt = await debtModel.findById(request.params.id);
-    response.json(debt);
-  } catch (error) {
-    response.status(500).send(error);
-  }
+  const debt = await debtModel.findById(request.params.id);
+  response.json(debt);
 };
 
-// Adds a debt between two users.
+// Add a debt between two users.
 exports.addDebt = async (request, response) => {
-  // Check whether the debt exists between two users so that we can either
-  // create a new debt, or update an existing debt.
-  const debtExists = await debtModel.exists({
-    from: request.body.from,
-    to: request.body.to,
-  });
-
-  try {
-    if (debtExists) {
-      // Update the debt between the lender and borrower.
-      helpers.updateDebt(
-        request.body.from,
-        request.body.to,
-        request.body.amount
-      );
-      response.send("Debt updated successfully.");
-    } else {
-      // Create a new debt between the lender and borrower.
-      helpers.createDebt(
-        request.body.from,
-        request.body.to,
-        request.body.amount
-      );
-      response.send("Debt created successfully.");
-    }
-  } catch (error) {
-    response.status(500).send(error);
-  }
+  let message = await helpers.processNewDebt(
+    request.body.from,
+    request.body.to,
+    request.body.amount
+  );
+  response.send(message);
 };
 
-// Settles a debt by ID.
+// Settle a debt by ID.
 exports.settleDebt = async (request, response) => {
   // Search for a debt by swapping the from and to fields, as a settlement is
   // effectively a reverse debt.
@@ -67,32 +37,30 @@ exports.settleDebt = async (request, response) => {
   } else if (existingDebt && existingDebt.amount > request.body.amount) {
     // If the existing debt is greater than the amount to be settled, then
     // reduce the debt.
-    try {
-      await debtModel.findOneAndUpdate(
-        {
-          from: request.body.to,
-          to: request.body.from,
-        },
-        {
-          $inc: { amount: -request.body.amount },
-        }
-      );
-      response.send("Debt partially settled and reduced successfully.");
-    } catch (error) {
-      response.status(500).send(error);
-    }
-  } else if (existingDebt && existingDebt.amount === request.body.amount) {
-    try {
-      // If the existing debt is equal to the amount to be settled, then delete
-      // the debt.
-      await debtModel.findOneAndDelete({
+    await debtModel.findOneAndUpdate(
+      {
         from: request.body.to,
         to: request.body.from,
-      });
-      response.send("Debt fully settled and deleted successfully.");
-    } catch (error) {
-      response.status(500).send(error);
-    }
+      },
+      {
+        $inc: { amount: -request.body.amount },
+      }
+    );
+    response.send(
+      `Debt from '${request.body.from}' to '${request.body.to}' partially\
+        settled and reduced successfully.`
+    );
+  } else if (existingDebt && existingDebt.amount === request.body.amount) {
+    // If the existing debt is equal to the amount to be settled, then delete
+    // the debt.
+    await debtModel.findOneAndDelete({
+      from: request.body.to,
+      to: request.body.from,
+    });
+    response.send(
+      `Debt from '${request.body.from}' to '${request.body.to}' fully\
+        settled and deleted successfully.`
+    );
   } else {
     response
       .status(400)
