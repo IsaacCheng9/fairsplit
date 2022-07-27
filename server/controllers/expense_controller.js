609ddea1c51a96c56a7f80dd43b8111f7603499f
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-07-27 22:03:37 +0100

    Add validation checks for borrower list

--- server/controllers/expense_controller.js
+++ server/controllers/expense_controller.js
@@ -1,8 +1,25 @@
 const helpers = require("./helpers/index");
 const expenseModel = require("../models/expense");
 
-// Adds an expense.
+// Add an expense and create the related debts between lender and borrowers.
 exports.addExpense = async (request, response) => {
+  // Check that the sum of amounts owed matches the total expense amount.
+  let currentSum = 0;
+  for (borrower of request.body.borrowers) {
+    if (borrower[1] <= 0) {
+      return response.status(400).json({
+        error: "The amount owed must be a positive number.",
+      });
+    } else {
+      currentSum += borrower[1];
+    }
+  }
+  if (currentSum !== request.body.amount) {
+    return response.status(400).json({
+      error: "The sum of amounts owed must equal the total expense amount.",
+    });
+  }
+
   // Keep a record of the expense for the history.
   const expense = await expenseModel.create({
     title: request.body.title,
