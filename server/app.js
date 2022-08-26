47be7f5c725c91922ca61de37c16950a8f72b4a4
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-08-26 22:29:01 +0100

    Fix IMPORTANT message in comment

--- server/app.js
+++ server/app.js
@@ -17,11 +17,11 @@ app.use(usersRouter);
 app.use(expensesRouter);
 app.use(debtsRouter);
 
-// IMPORTANT: Create .env file with password
+// !IMPORTANT: Create .env file with password
 const password = process.env.PASSWORD || "changePasswordHere";
 let devUrl = `mongodb+srv://admin:${password}@fairsplit.fjvgxmg.mongodb.net/?retryWrites=true&w=majority`;
 var mongoDB = process.env.MONGODB_URI || devUrl;
-// Sets up the Mongoose connection.
+// Set up the Mongoose connection.
 mongoose
   .connect(mongoDB, {
     useNewUrlParser: true,
@@ -30,7 +30,7 @@ mongoose
   .catch((error) => handleError(error));
 let db = mongoose.connection;
 
-// Binds the connection to an error event to get notification of connection
+// Bind the connection to an error event to get notification of connection
 // errors.
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // Displays a success message when the connection is successfully made.
@@ -38,7 +38,7 @@ db.once("open", function () {
   console.log("Connected successfully.");
 });
 
-// Sets up the port to listen on.
+// Set up the port to listen on.
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running at port ${port}.`);
