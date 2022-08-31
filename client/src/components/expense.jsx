b657cc57fe53aa55340b01bb68a7bd78788e7122
George Kombostiotis <71034989+gkSideProjects@users.noreply.github.com>
2022-08-31 09:59:50 +0100

    Fix user creation, add ability to expand expense (#65)
    * Fix for user creation
    
    * Fix overflowing expense text
    
    * Add div to each expense, expands when clicked
    
    * Create function to calculate height of div
    
    * Move border to bottom of expense
    
    * Add arrow to expense, displaying ability to expand

--- client/src/components/expense.jsx
+++ client/src/components/expense.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { React, useState } from "react";
 import "../styles/expense.css";
 import smallArrow from "../assets/small_arrow.svg";
 
 function Expense(props) {
+  let [dynamicHeight, setDynamicHeight] = useState(0);
+
+  // Calculate conatiner height based on number of borrowers
+  function expandContainer() {
+    if (dynamicHeight) {
+      setDynamicHeight(0);
+    } else {
+      for (let i = 0; i < props.value.borrowers.length; i++) {
+        if (i % 2 === 0) {
+          dynamicHeight += 1.86;
+        }
+      }
+      setDynamicHeight(dynamicHeight);
+    }
+  }
+
   // Format borrowers with commas
   function renderBorrowers() {
     const borrowers = [];
@@ -14,19 +30,82 @@ function Expense(props) {
   }
 
   return (
-    <div className="expense">
-      <div className="e-date">
-        {props.value.creationDatetime.substring(0, 10)}
-      </div>
-      <div className="e-pic"></div>
-      <div className="e-title">{props.value.title}</div>
-      <div className="e-member-price">
-        <div className="e-price">£{(props.value.amount / 100).toFixed(2)}</div>
-        <div className="e-members">
-          {props.value.lender}
-          <img alt="arrow" className="e-arrow" src={smallArrow}></img>
-          {renderBorrowers()}
+    <div onClick={expandContainer}>
+      <div className="expense">
+        <div className="e-date">
+          {props.value.creationDatetime.substring(0, 10)}
         </div>
+        <div className="e-pic"></div>
+        <div className="e-title">{props.value.title}</div>
+        <div className="e-member-price">
+          <div className="e-price">
+            £{(props.value.amount / 100).toFixed(2)}
+          </div>
+          <div className="e-members">
+            <div>{props.value.lender}</div>
+            <img alt="arrow" className="e-arrow" src={smallArrow}></img>
+            <div
+              style={{
+                overflow: "hidden",
+                whiteSpace: "nowrap",
+                textOverflow: "ellipsis",
+              }}
+            >
+              {renderBorrowers()}
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            justifySelf: "center",
+            backgroundColor: "lightgrey",
+            borderRadius: "5px",
+            width: "1em",
+            height: "1.2em",
+            padding: "0.1em",
+          }}
+        >
+          <img
+            style={{
+              width: "1em",
+              height: "1.2em",
+              transform: "rotateZ(90deg)",
+            }}
+            alt="arrow"
+            src={smallArrow}
+          ></img>
+        </div>
+      </div>
+      <div
+        style={{
+          maxHeight: dynamicHeight + "em",
+          display: "flex",
+          flexFlow: "wrap",
+          paddingBottom: "0.2em",
+        }}
+        className="expand-expense"
+      >
+        {props.value.borrowers.map((borrower) => {
+          return (
+            <div className="e-member-price">
+              <div
+                className="e-members"
+                style={{
+                  width: "auto",
+                  margin: "0.3em",
+                  backgroundColor: "lightgrey",
+                }}
+                key={borrower[0]}
+              >
+                {borrower[0]}
+                <img alt="arrow" className="e-arrow" src={smallArrow}></img>
+                <div className="e-price" key={borrower[0] + borrower[1]}>
+                  {"£" + (borrower[1] / 100).toFixed(2)}
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
