684f3140fd045e5a1cfcf53e9098dc58ef5c5e51
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-08-31 14:46:39 +0100

    Rename file, add /users/add test

--- server/tests/test.js
+++ server/tests/test.js
@@ -1,4 +1,4 @@
-const app = require("../server");
+const app = require("../app");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 
@@ -17,3 +17,16 @@ afterEach((done) => {
     mongoose.connection.close(() => done());
   });
 });
+
+// TODO: Test /users/add endpoint.
+test("Test /users/add endpoint", async () => {
+  const response = await supertest(app).post("/users/add").send({
+    username: "test",
+    firstName: "Test",
+    lastName: "User",
+  });
+  expect(response.statusCode).toBe(200);
+  expect(response.body.username).toBe("test");
+  expect(response.body.firstName).toBe("Test");
+  expect(response.body.lastName).toBe("User");
+}).timeout(10000);
