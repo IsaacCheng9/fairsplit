1745eb90a3350f351d0bfad95212ae106e70617d
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-09-02 04:34:09 +0100

    Add test to GET /users

--- server/tests/test.js
+++ server/tests/test.js
@@ -1,32 +1,9 @@
 const app = require("../app");
-const mongoose = require("mongoose");
 const supertest = require("supertest");
 
-// Connect to MongoDB before running each test.
-beforeEach((done) => {
-  mongoose.connect(
-    "mongodb://localhost:27017/JestDB",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => done()
-  );
-});
+// Use the Supertest object to make requests to the app.
+const api = supertest(app);
 
-// Remove all data from the database after each test.
-afterEach((done) => {
-  mongoose.connection.db.dropDatabase(() => {
-    mongoose.connection.close(() => done());
-  });
+test("GET /users", async () => {
+  await api.get("/users").expect(200);
 });
-
-// TODO: Test /users/add endpoint.
-test("Test /users/add endpoint", async () => {
-  const response = await supertest(app).post("/users/add").send({
-    username: "test",
-    firstName: "Test",
-    lastName: "User",
-  });
-  expect(response.statusCode).toBe(200);
-  expect(response.body.username).toBe("test");
-  expect(response.body.firstName).toBe("Test");
-  expect(response.body.lastName).toBe("User");
-}).timeout(10000);
