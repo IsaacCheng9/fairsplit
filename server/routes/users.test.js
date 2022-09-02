fc70e721ea651d760ed8645375bf48cbf58748e3
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-09-02 04:56:18 +0100

    Add afterAll to close connection

--- server/routes/users.test.js
+++ server/routes/users.test.js
@@ -1,9 +1,15 @@
 const app = require("../app");
 const supertest = require("supertest");
+const mongoose = require("mongoose");
 
 // Use the Supertest object to make requests to the app.
 const api = supertest(app);
 
 test("GET /users", async () => {
   await api.get("/users").expect(200);
+});
+
+afterAll((done) => {
+  mongoose.connection.close();
+  done();
 });
