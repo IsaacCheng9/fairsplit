2f2a858b3756149c3b36a1855e422ff5e4d59670
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-09-14 03:25:41 +0100

    Create tests for user endpoints

--- server/routes/users.test.js
+++ server/routes/users.test.js
@@ -2,14 +2,40 @@ const app = require("../app");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 
+const userModel = require("../models/user");
+const userDebtModel = require("../models/user_debt");
+
 // Use the Supertest object to make requests to the app.
 const api = supertest(app);
 
+// Check whether we can get a list of all users.
 test("GET /users", async () => {
   await api.get("/users").expect(200);
 });
 
-afterAll((done) => {
+// Check whether we can create a new user.
+test("POST /users", async () => {
+  await api
+    .post("/users")
+    .send({
+      username: "testuser123",
+      firstName: "Test",
+      lastName: "User",
+    })
+    .expect(201);
+});
+
+// Check whether our user was created.
+test("GET /users/:username", async () => {
+  await api.get("/users/testuser123").expect(200);
+});
+
+afterAll(async () => {
+  // TODO: Create routes to delete users and user debts.
+  // Delete the user we created.
+  await userModel.deleteOne({ username: "testuser123" });
+  // Delete the debt record for the user we created.
+  await userDebtModel.deleteOne({ username: "testuser123" });
+
   mongoose.connection.close();
-  done();
 });
