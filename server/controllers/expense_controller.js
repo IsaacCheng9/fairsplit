52eb1c22ac941fff691ac9c0d5caec8e7df46d78
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-09-24 16:31:32 +0100

    Send RC 201 instead of 200 after creating expense

--- server/controllers/expense_controller.js
+++ server/controllers/expense_controller.js
@@ -42,7 +42,7 @@ exports.addExpense = async (request, response) => {
   // Recalculate debts to minimise the number of transactions, as this
   // settlement may have changed the optimal strategy.
   helpers.simplifyDebts();
-  response.json(expense);
+  response.status(201).json(expense);
 };
 
 // Get all expenses.
