5f9021dc49da7a1dc8b965cd932f7c99f25e578e
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-09-24 22:58:35 +0100

    Add unit tests for user routes (#73)
    * Install Jest, SuperTest
    
    * Add test command using Jest
    
    * Add file for testing endpoints
    
    * Rename file, add /users/add test
    
    * Install Jest as a dev dependency only
    
    * Add test to GET /users
    
    * Format code with Prettier
    
    * Move test to same folder as code
    
    * Add workflow for Node.js CI
    
    * Run node.yml on push
    
    * Change directory to /server at the start
    
    * Add cache-dependency-path
    
    * Remove old code to change directory
    
    * Remove node-version 12.x
    
    * Add afterAll to close connection
    
    * Temporarily force Jest to exit after completing
    
    * Add step to start a MongoDB server
    
    * Remove pipeline to run unit tests
    
    * Change successful user creation RC 200 -> 201
    
    * Change route to create user, get user by username
    
    * Create tests for user endpoints
    
    * Add endpoint to delete user by their username
    
    * Create test for deleting a user
    
    * Remove old TODO message
    
    Co-authored-by: IsaacCheng9 <IsaacCheng9@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -43,3 +43,5 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running at port ${port}.`);
 });
+
+module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,19 +2,16 @@ const express = require("express");
 const userController = require("../controllers/user_controller");
 const app = express();
 
-// Gets a list of all the users.
+// Get a list of all the users.
 app.get("/users", userController.getUsers);
 
-// Gets a user by ID.
-app.get("/users/:id", userController.getUserById);
+// Create a new user.
+app.post("/users", userController.addUser);
 
-// Creates a new user.
-app.post("/users/add", userController.addUser);
+// Get a user by their username.
+app.get("/users/:username", userController.getUserByUsername);
 
-// Updates a user by ID.
-// app.put("/users/update/:id", userController.updateUser);
-
-// Deletes a user by ID.
-// app.delete("/users/delete/:id", userController.deleteUser);
+// Delete a user by their username.
+app.delete("/users/:username", userController.deleteUser);
 
 module.exports = app;
