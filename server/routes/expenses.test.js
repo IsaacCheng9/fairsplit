298d2c828c05e7d1fefd4cc817d219ee208b30a6
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-09-26 18:28:24 +0100

    Add describe block for expense route testing

--- server/routes/expenses.test.js
+++ server/routes/expenses.test.js
@@ -1,62 +1,76 @@
-const app = require("../app");
+const express = require("express");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 
+const expensesRouter = require("../routes/expenses.js");
 const expenseModel = require("../models/expense");
 
-// Use the Supertest object to make requests to the app.
-const api = supertest(app);
+describe("Test for expense routes", () => {
+  const app = express();
+  app.use(express.json());
+  app.use(expensesRouter);
 
-// Check whether we can get a list of all expenses.
-test("GET /expenses", async () => {
-  await api.get("/expenses").expect(200);
-});
+  beforeAll(async () => {
+    // Connect to the mock database.
+    connection = await mongoose.connect(globalThis.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
 
-// Check whether we can add an expense between two users.
-test("POST /expenses", async () => {
-  await api
-    .post("/expenses")
-    .send({
+  afterAll(async () => {
+    // Delete the expense we created.
+    await expenseModel.deleteOne({
       title: "testexpense",
       author: "testuser123",
       lender: "testuser123",
       borrowers: [["testuser456", 100]],
       amount: 100,
-    })
-    .expect(201);
-});
-
-// Check whether we can add a settlement between two users as an expense.
-test("POST /expenses/settlement", async () => {
-  await api
-    .post("/expenses/settlement")
-    .send({
+    });
+    // Delete the settlement expense we created.
+    await expenseModel.deleteOne({
       title: "SETTLEMENT",
       author: "testuser456",
       lender: "testuser456",
       borrowers: [["testuser123", 100]],
       amount: 100,
-    })
-    .expect(201);
-});
+    });
+    mongoose.connection.close();
+  });
 
-afterAll(async () => {
-  // Delete the expense we created.
-  await expenseModel.deleteOne({
-    title: "testexpense",
-    author: "testuser123",
-    lender: "testuser123",
-    borrowers: [["testuser456", 100]],
-    amount: 100,
+  // Check whether we can get a list of all expenses.
+  test("GET /expenses", async () => {
+    const server = supertest(app);
+    await server.get("/expenses").expect(200);
   });
-  // Delete the settlement expense we created.
-  await expenseModel.deleteOne({
-    title: "SETTLEMENT",
-    author: "testuser456",
-    lender: "testuser456",
-    borrowers: [["testuser123", 100]],
-    amount: 100,
+
+  // Check whether we can add an expense between two users.
+  test("POST /expenses", async () => {
+    const server = supertest(app);
+    await server
+      .post("/expenses")
+      .send({
+        title: "testexpense",
+        author: "testuser123",
+        lender: "testuser123",
+        borrowers: [["testuser456", 100]],
+        amount: 100,
+      })
+      .expect(201);
   });
-  // TODO: Fix open handles.
-  mongoose.connection.close();
+
+  // Check whether we can add a settlement between two users as an expense.
+  test("POST /expenses/settlement", async () => {
+    const server = supertest(app);
+    await server
+      .post("/expenses/settlement")
+      .send({
+        title: "SETTLEMENT",
+        author: "testuser456",
+        lender: "testuser456",
+        borrowers: [["testuser123", 100]],
+        amount: 100,
+      })
+      .expect(201);
+  });
 });
